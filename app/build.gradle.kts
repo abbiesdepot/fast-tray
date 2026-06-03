81e0fc5d7cc5c50a3baded5b95d3dae1f8511611
Grcllch02 <gchelsea01@student.ciputra.ac.id>
2026-06-04 01:58:08 +0700

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+import jdk.tools.jlink.resources.plugins
+import sun.awt.windows.awtLocalization_sv
+
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    awtLocalization_sv(libs.plugins.android.application)
+    awtLocalization_sv(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
 
